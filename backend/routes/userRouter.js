ba1d9c0d9cedb4bd29055a33ff94a90b5f91df8e
Prasad Joshi <ppjoshi2003@gmail.com>
2024-08-09 23:02:51 +0530

    slightly ugly solution for signup route

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const {z} = require("zod");
-
+const jwt = require("jsonwebtoken");
 
 router.post('/signup', (req, res, next) => {
-    
+    const {password, firstName, lastName, userName} = req.body;
+    const validatedPassword = z.string().min(6);
+    const validateFirstName = z.string().min(2);
+    const validateLastName = z.string().min(2);
+    const validateUserName = z.string();
+
+    const result = (validatedPassword.safeParse(password) && validateFirstName.safeParse(firstName) && validateLastName.safeParse(lastName) && validateUserName.safeParse(userName))
+    if(!result.success){
+        res.status(411).json({
+            msg: "enter proper credentials"
+        });
+    }
+    else{
+        const JWTtoken = jwt.sign(userName, password);
+        res.status(200).json({
+            token: JWTtoken,
+            msg: "user signed up successfully"
+        })
+    }
 })
