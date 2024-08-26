5c6ba305934da9699156b594c0ec93e1255c882a
Prasad Joshi <ppjoshi2003@gmail.com>
2024-08-26 23:51:54 +0530

    creating from scratch on new branch

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-// backend/index.js
-const express = require('express');
-const cors = require("cors");
-const rootRouter = require("./routes/index");
-
+const express = require("express");
 const app = express();
 
-app.use(cors());
+const router = express.Router();
+
 app.use(express.json());
 
-app.use("/api/v1", rootRouter);
+router.use("/api/v1", mainRouter)
 
-app.listen(3000);
+
+app.listen(3000, () => {
+    console.log("app listening on port 3000")
+})
